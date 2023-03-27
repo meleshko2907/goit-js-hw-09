894018e74a1e1199a17f5d0ef22cd57e61fdd54c
Meleshko Aleksey <meleshko.aleksey1996@gmail.com>
2023-03-28 01:57:51 +0300

    add first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,32 @@
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+let intervalId = null;
 
+console.log(stopBtn);
+console.log(startBtn);
+
+startBtn.addEventListener('click', () => {
+    intervalId = setInterval(() => {
+    const hexColor = getRandomHexColor();
+    document.body.style.background = hexColor;
+    startBtn.setAttribute('disabled', true);
+    if (intervalId){
+        stopBtn.removeAttribute('disabled');
+    }
+}, 1000);
+});
+
+stopBtn.addEventListener('click', () => {
+    clearInterval(intervalId);
+    startBtn.removeAttribute('disabled');
+  
+    if (intervalId) {
+      stopBtn.setAttribute('disabled', true);
+    }
+  });
+
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
